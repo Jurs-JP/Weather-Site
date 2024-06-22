2ba28ba73df4376ad6036170fa129839ab68a61f
Jurist John Plaza <40057871+Jurs-JP@users.noreply.github.com>
2024-06-22 12:07:33 +0800

    Add files via upload
    Added the last codes for texts and numbers linked to their API converted values. Missing a bit on design.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let searchNum = 0;
 window.scrollTo(0,0)
 
+const pageBody = document.querySelector("body")
+
+const cityInput = document.querySelector("#cityName");
 const city = document.querySelector("#city");
 const dateNow = document.querySelector("#dateNow")
 const weatherIcon = document.querySelector("#weatherIcon");
@@ -14,30 +17,27 @@ const precipitation = document.querySelector("#precipitationValue")
 const humidity = document.querySelector("#humidityValue")
 const cloud = document.querySelector("#cloudValue")
 
-
 const hourlyArr = document.querySelectorAll("#todayHourlyWeather > *")
 const hourlyTempText = document.querySelectorAll("#todayHourlyWeather > * > *:first-child > *:first-child")
 const hourlyTempGraph = document.querySelectorAll("#todayHourlyWeather > * > *:first-child > *:last-child")
 const hourlyTime = document.querySelectorAll("#todayHourlyWeather > * > *:last-child")
 
-// const hourlyObject = {
-//     1: {
-//         time: 0,
-//         temp: 0
-//         },  
-//     2: {
-//         time: 0,
-//         temp: 0
-//         }
+const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-// }
+cityInput.addEventListener("keypress", function(event) {
 
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.querySelector("#searchCity").click();
+    }
+  });
 
 let searchCity = (e) => {
-    const input = document.querySelector("#cityName");
-    const inputValue = input.value;
+
+    const inputValue = cityInput.value;
 
     getWeather(inputValue)
+    pageBody.style.backgroundImage = `linear-gradient(10deg, rgb(4, 99, 202), rgb(4, 135, 226))`
 
     if(searchNum == 0) {
         const searchBox = document.querySelector("#searchBox");
@@ -51,9 +51,6 @@ let searchCity = (e) => {
     }
 
     searchNum++
-
-    // city.textContent = data.res.data
-    // console.log(res)
 }
 
 const getWeather = async (id) => {
@@ -65,10 +62,8 @@ const getWeather = async (id) => {
         
         let latitude = (res.data.location.lat)
         let longitude = (res.data.location.lon)
-        console.log(res)
-        weatherToday(latitude, longitude)
-
         
+        weatherToday(latitude, longitude)
         
         let cityResult = (res.data.location.region)
         let countryResult = (res.data.location.country)
@@ -82,7 +77,6 @@ const getWeather = async (id) => {
         let precipitationResult = (res.data.current.precip_mm)
         let humidityResult = (res.data.current.humidity)
         let cloudResult = (res.data.current.cloud)
-        
         
         let conditionResult = (res.data.current.condition.text)
 
@@ -109,38 +103,87 @@ const getWeather = async (id) => {
 
 const weatherToday = async (latitude, longitude) => {
     try {
-    const config2 = {headers: {Accept: 'application/json'}}
-    const res2 = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=be513ddf6e7ac5604545cdad8706134f`, config2);
-                                
-    console.log(res2)
+        const config2 = {headers: {Accept: 'application/json'}}
+        const res2 = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=be513ddf6e7ac5604545cdad8706134f`, config2);                        
 
-
-    for(let i = 0; i < hourlyArr.length; i++) {
-        
-        if (res2.data.list[i].dt_txt.slice(11,13) >= 12) {
-            if (res2.data.list[i].dt_txt.slice(11,13) > 12) {
-                hourlyTime[i].innerHTML = `${res2.data.list[i].dt_txt.slice(11,13)-12} PM`
-            } else { hourlyTime[i].innerHTML = `${res2.data.list[i].dt_txt.slice(11,13)} PM` }
-        } else {
-            if (res2.data.list[i].dt_txt.slice(11,13) == 0) {
-                hourlyTime[i].innerHTML= `12 AM`
+        for(let i = 0; i < hourlyArr.length; i++) {
+            
+            if (res2.data.list[i].dt_txt.slice(11,13) >= 12) {
+                if (res2.data.list[i].dt_txt.slice(11,13) > 12) {
+                    hourlyTime[i].innerHTML = `${res2.data.list[i].dt_txt.slice(11,13)-12} PM`
+                } else { hourlyTime[i].innerHTML = `${res2.data.list[i].dt_txt.slice(11,13)} PM` }
             } else {
-                hourlyTime[i].innerHTML= `${res2.data.list[i].dt_txt.slice(12,13)} AM`
+                if (res2.data.list[i].dt_txt.slice(11,13) == 0) {
+                    hourlyTime[i].innerHTML= `12 AM`
+                } else {
+                    hourlyTime[i].innerHTML= `${res2.data.list[i].dt_txt.slice(12,13)} AM`
+                }
+            }
+            
+            hourlyTempText[i].innerHTML = `${((res2.data.list[i].main.temp)-273.15).toFixed(1)}°`
+            hourlyTempGraph[i].style.height = `${(((res2.data.list[i].main.temp)-273.15).toFixed(1))*2}%`
+            hourlyTempGraph[i].style.backgroundColor = `rgb(${((res2.data.list[i].main.temp)-273.15).toFixed(1)*5},0,${255 - (((res2.data.list[i].main.temp)-273.15).toFixed(1)*5)})`
+        }
+       
+        const futureWeather = document.querySelector("#futureWeather")
+
+        if (futureWeather.childNodes.length < 4) {
+            for (let i = 0; i < 4; i++) {
+                const futureDaysForecast = document.createElement("div")
+              
+                document.querySelector("#futureWeather").appendChild(futureDaysForecast)
             }
         }
-         
-        hourlyTempText[i].innerHTML = `${((res2.data.list[i].main.temp)-273.15).toFixed(1)}°`
-        hourlyTempGraph[i].style.height = `${(((res2.data.list[i].main.temp)-273.15).toFixed(1))*2}%`
-        hourlyTempGraph[i].style.backgroundColor = `rgb(${((res2.data.list[i].main.temp)-273.15).toFixed(1)*5},0,${255 - (((res2.data.list[i].main.temp)-273.15).toFixed(1)*5)})`
-    }
-    
+
+       const futureDays = document.querySelectorAll("#futureWeather > * ")
+      
+       
+       for (let i = 0; i < futureDays.length; i++) {
+
+            const birthday = new Date(res2.data.list[(i+1)*8].dt_txt);
+            const day1 = birthday.getDay();
+
+            futureDays[i].innerHTML = `${dayOfWeek[day1]} ${res2.data.list[(i+1)*8].dt_txt.slice(8, 10)} <div><div></div><div><div></div><div></div></div></div>`
+           
+            const futureDaysMaxTemp = document.querySelectorAll("#futureWeather > * > * > *:last-child > *:first-child")
+            const futureDaysMinTemp = document.querySelectorAll("#futureWeather > * > * > *:last-child > *:last-child")
+            
+            let tempArray = []
+
+            for(let e = 0; e <=4; e++) {
+                tempArray.push((res2.data.list[((i+1)*8)+e].main.temp))
+            }
+            
+            futureDaysMaxTemp[i].innerHTML = `${((Math.max(...tempArray))-273.15).toFixed(1)}°`
+            futureDaysMinTemp[i].innerHTML = `${((Math.min(...tempArray))-273.15).toFixed(1)}°`
+            
+            futureWeatherIcon(res2.data.list[(i+1)*8].weather[0].icon, i)
+       }
+
+       
     }  
     catch {
-    console.log("Geolocation Error!")
-    console.log(latitude)
-    console.log(longitude)
+        console.log("Geolocation Error!")
+        console.log(latitude)
+        console.log(longitude)
     }
 }
 
+const futureWeatherIcon = async (icon, num) => {
+    try{
+        const config3 = {headers: {Accept: 'application/json'}}
+        const res3 = await axios.get(`https://openweathermap.org/img/wn/${icon}@2x.png`, config3);
+        
+        const futureDays = document.querySelectorAll("#futureWeather > * ")
+        const futureDaysIcon = document.querySelectorAll("#futureWeather > * > * > *:first-child")
+
+       for (let i = 0; i < futureDays.length; i++) {
+            futureDaysIcon[num].style.backgroundImage = `url(${res3.config.url})`
+       }
+    } 
+    catch {
+        console.log("Future Weather Not Available")
+    }
+}
 
 
